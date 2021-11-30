470fcd4670d79c38b4d6433af2c6f431b9dafc76
pspspss <paulo.pspspss@gmail.com>
2021-11-29 22:07:20 -0300

    Implementing concept using functions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use rusqlite;
+// use std::fs;
+use std::io::{self, prelude::*, BufReader};
 
 #[derive(Debug)]
 struct Test {
@@ -6,7 +8,78 @@ struct Test {
     description: String,
 }
 
+// enum SqliteTypes {
+// Text(String),
+// Int(i32),
+// Float(f32),
+// }
+//
+// struct SqliteWrapper {
+// table_name: String,
+// conn: rusqlite::Connection,
+// }
+//
+// impl SqliteWrapper {
+// fn new(conn: rusqlite::Connection, table_name: String) -> Self {
+// Self { conn, table_name }
+// }
+//
+// fn gen_table(&self) {}
+//
+// fn gen_populate(&self) {}
+// }
+
+fn usage() {
+    print!("Usage ./rq <query>\n");
+}
+
+fn find_tables(query: &str) -> Vec<&str> {
+    let (_, tables) =
+        query
+            .split_whitespace()
+            .fold(("", Vec::new()), |(last, mut result), current| {
+                match last {
+                    "from" => {
+                        result.push(current);
+                    }
+                    _ => {}
+                }
+                (current, result)
+            });
+
+    tables
+}
+
+fn build_table(file_path: &str) {
+    let file = std::fs::File::open(file_path).unwrap();
+    let reader = std::io::BufReader::new(file);
+
+    reader.lines().
+
+    for line in reader.lines() {
+        println!("{}", line.unwrap());
+    }
+}
+
 fn main() {
+    let args: Vec<_> = std::env::args().collect();
+    if args.len() < 2 {
+        usage();
+        return;
+    }
+
+    let query = &args[1];
+
+    println!("{}", query);
+    let tables = find_tables(query);
+    let sample_table = tables.first().unwrap();
+    println!("{}", sample_table);
+    build_table(sample_table);
+    // let data = fs::read_to_string(query).expect("Unable to read file");
+    // println!("{}", data);
+}
+
+fn _main() {
     let conn = rusqlite::Connection::open_in_memory().unwrap();
 
     conn.execute(
